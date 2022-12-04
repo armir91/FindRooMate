884278ab2b60e2cb2aeeacf6e91d5c324b89f2b4
Avjol Sakaj <avjolsakaj@outlook.com>
2022-12-04 10:24:48 +0100

    Add DAL and BLL folder

--- FindRooMateApi/Entities/FindRooMateContext.cs
+++ FindRooMateApi/Entities/FindRooMateContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace FindRooMateApi.Entities
 {
@@ -28,7 +25,7 @@ namespace FindRooMateApi.Entities
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=armir;Database=FindRooMate;Trusted_Connection=True;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=AVJOL-DELL\\SQLEXPRESS01;Database=FindRooMate;Trusted_Connection=True;TrustServerCertificate=True");
             }
         }
 
